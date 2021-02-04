f7f9077420e469f35a769365440f294ed0d61f5e
Chris <le.liquideur01@gmail.com>
2021-02-04 21:52:18 +0100

    création du service book

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Book } from '../models/Book.model';
+import {Subject} from 'rxjs/Subject';
+import firebase from "firebase/app";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
-
+  books: Book[]=[];
+  bookSubject = new Subject<Book[]>();
   constructor() { }
+  emitBooks(){
+    this.bookSubject.next(this.books);
+  }
+  saveBooks(){
+    firebase.database().ref('/books').set(this.books);
+
+  }
+  getBooks(){
+    firebase.database().ref('/books')
+    .on('value',(data)=>{
+      this.books=data.val()?data.val():[];
+      this.emitBooks();
+    })
+  }
+  getingleBook(id:number){
+    return new Promise(
+      (resolve, reject)=>{
+        firebase.database().ref('books/'+id).once('value').then(
+          (data)=>{
+            resolve(data.val())
+          },(error)=>{
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+  createNewBook(newBook:Book){
+    this.books.push(newBook);
+    this.saveBooks();
+    this.emitBooks();
+  }
+  removeBook(book:Book){
+    const bookindexToRemove = this.books.findIndex(
+      (bookEl)=>{
+        if(bookEl===book){
+          return true;
+        }
+      }
+    );
+    this.books.splice(bookindexToRemove,1);
+    this.saveBooks();
+    this.emitBooks();
+  }
 }
