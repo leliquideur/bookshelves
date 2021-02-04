4f7655e23f8ec624864c9abc904e42b12e6684c2
Chris <le.liquideur01@gmail.com>
2021-02-04 22:25:27 +0100

    book list ts & template ok

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Book } from '../models/Book.model';
+import {Subscription} from 'rxjs/Subscription';
+import {BooksService} from '../services/books.service'
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
+  books : Book[];
+  bookSubscription: Subscription;
 
-  constructor() { }
+  constructor(private booksService: BooksService, private router: Router) { }
 
   ngOnInit(): void {
+    this.bookSubscription=this.booksService.bookSubject.subscribe(
+      (books: Book[])=>{
+        this.books=books;
+      }
+    );
+    this.booksService.emitBooks();
   }
-
+  onNewBook(){
+    this.router.navigate(['/books','new']);
+  }
+  onDeleteBook(book: Book){
+    this.booksService.removeBook(book);
+  }
+  onViewBook(id: number){
+    this.router.navigate(['/books','view',id]);
+  }
+  ngOnDestroy(): void {
+    this.bookSubscription.unsubscribe();
+  }
 }
